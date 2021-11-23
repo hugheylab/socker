85650322e74c2501d5a2b11e346900bd3dbab403
Jake Hughey <jakejhughey@gmail.com>
2021-11-23 15:43:06 -0600

    simplified to build from stocker.

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,8 @@
-FROM rocker/tidyverse:4.1.2
+FROM ghcr.io/hugheylab/stocker:latest
 
 LABEL org.opencontainers.image.authors="Jake Hughey <jakejhughey@gmail.com>" \
       org.opencontainers.image.source="https://github.com/hugheylab/socker"
 
-RUN apt-get update && \
-  apt-get install -y \
-    nano \
-    tmux && \
-  rm -rf /var/lib/apt/lists/*
-
-# RUN usermod -aG sudo rstudio
-# user can set password with "sudo passwd rstudio"
-
-USER rstudio
-WORKDIR /home/rstudio
 COPY install_seeker.sh .
 RUN ./install_seeker.sh
 ENV REFGENIE "/home/rstudio/genomes/genome_config.yaml"
-
-CMD /bin/bash
--- install_seeker.sh
+++ install_seeker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 Rscript -e "if (!requireNamespace('BiocManager', quietly = TRUE)) install.packages('BiocManager')"
@@ -13,27 +12,23 @@ tar -zxvf ibm-aspera-connect_4.1.0.46-linux_x86_64.tar.gz
 ./ibm-aspera-connect_4.1.0.46-linux_x86_64.sh
 rm ibm-aspera-connect*
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sh Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
-rm Miniconda3-latest-Linux-x86_64.sh
-~/miniconda3/bin/conda init bash
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# sh Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
+# rm Miniconda3-latest-Linux-x86_64.sh
+# ~/miniconda3/bin/conda init bash
 
 source ~/miniconda3/etc/profile.d/conda.sh
-conda config --set auto_activate_base false
+# conda config --set auto_activate_base false
 conda activate
 
-conda config --add channels defaults
-conda config --add channels bioconda
-conda config --add channels conda-forge
+# conda config --add channels defaults
+# conda config --add channels bioconda
+# conda config --add channels conda-forge
 
-conda install mamba -c conda-forge
+# conda install mamba -c conda-forge
 mamba install refgenie trim-galore fastqc fastq-screen salmon multiqc
 
 mkdir -p ~/genomes
 refgenie init -c ~/genomes/genome_config.yaml
 echo 'export REFGENIE="${HOME}/genomes/genome_config.yaml"' >> ~/.bashrc
 echo 'export REFGENIE="${HOME}/genomes/genome_config.yaml"' >> ~/.profile
-
-# user can then run:
-# refgenie pull mm10/salmon_sa_index
-# refgenie pull mm10/salmon_partial_sa_index
