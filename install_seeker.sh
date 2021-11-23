6024c6817c5e5b74acf4cca558ba401be779e457
Jake Hughey <jakejhughey@gmail.com>
2021-11-23 15:58:41 -0600

    removed commented-out lines.

--- install_seeker.sh
+++ install_seeker.sh
@@ -12,21 +12,9 @@ tar -zxvf ibm-aspera-connect_4.1.0.46-linux_x86_64.tar.gz
 ./ibm-aspera-connect_4.1.0.46-linux_x86_64.sh
 rm ibm-aspera-connect*
 
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-# sh Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
-# rm Miniconda3-latest-Linux-x86_64.sh
-# ~/miniconda3/bin/conda init bash
-
 source ~/miniconda3/etc/profile.d/conda.sh
-# conda config --set auto_activate_base false
 conda activate
-
-# conda config --add channels defaults
-# conda config --add channels bioconda
-# conda config --add channels conda-forge
-
-# conda install mamba -c conda-forge
-mamba install refgenie trim-galore fastqc fastq-screen salmon multiqc
+mamba install fastq-screen fastqc multiqc refgenie salmon trim-galore
 
 mkdir -p ~/genomes
 refgenie init -c ~/genomes/genome_config.yaml
