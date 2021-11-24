2dfb0bade6f326d2f9531c66aa2d0940a9aeb47e
Jake Hughey <jakejhughey@gmail.com>
2021-11-23 18:28:32 -0600

    should actually be fixed now.

--- install_seeker.sh
+++ install_seeker.sh
@@ -2,8 +2,8 @@
 set -e
 
 Rscript -e "if (!requireNamespace('BiocManager', quietly = TRUE)) install.packages('BiocManager')"
-Rscript -e "BiocManager::install('preprocessCore', configure.args = '--disable-threading')" # https://support.bioconductor.org/p/122925/
 Rscript -e "BiocManager::install('seeker', site_repository = 'https://hugheylab.github.io/drat/', update = FALSE, ask = FALSE)"
+Rscript -e "BiocManager::install('preprocessCore', configure.args = '--disable-threading', force = TRUE)" # https://support.bioconductor.org/p/122925/
 Rscript -e "install.packages('doParallel')"
 rm -rf /tmp/downloaded_packages
 
