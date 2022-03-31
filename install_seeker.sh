e185306c52abab190b6301f31f421fceeeabb00b
Jake Hughey <jakejhughey@gmail.com>
2022-03-31 18:17:54 -0500

    switched from aspera to sra toolkit.

--- install_seeker.sh
+++ install_seeker.sh
@@ -10,14 +10,19 @@ Rscript -e "library('doParallel')"
 rm -rf /tmp/downloaded_packages
 
 # url and file name could change
-wget https://d3gcli72yxqn2z.cloudfront.net/connect_latest/v4/bin/ibm-aspera-connect_4.1.1.73_linux.tar.gz
-tar -zxvf ibm-aspera-connect_4.1.1.73_linux.tar.gz
-./ibm-aspera-connect_4.1.1.73_linux.sh
-rm ibm-aspera-connect*
+# wget https://d3gcli72yxqn2z.cloudfront.net/connect_latest/v4/bin/ibm-aspera-connect_4.1.1.73_linux.tar.gz
+# tar -zxvf ibm-aspera-connect_4.1.1.73_linux.tar.gz
+# ./ibm-aspera-connect_4.1.1.73_linux.sh
+# rm ibm-aspera-connect*
+wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.0.0/sratoolkit.3.0.0-ubuntu64.tar.gz
+tar -zxvf sratoolkit.3.0.0-ubuntu64.tar.gz
+echo 'export PATH="${PATH}:${HOME}/sratoolkit.3.0.0-ubuntu64/bin"' >> ~/.bashrc
+echo 'export PATH="${PATH}:${HOME}/sratoolkit.3.0.0-ubuntu64/bin"' >> ~/.profile
+rm sratoolkit*.tar.gz
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate
-mamba install fastq-screen fastqc multiqc refgenie salmon trim-galore
+mamba install fastq-screen fastqc multiqc pigz refgenie salmon trim-galore
 
 mkdir -p ~/genomes
 refgenie init -c ~/genomes/genome_config.yaml
