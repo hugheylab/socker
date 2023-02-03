228990e7dc94e878012353c05e8a4749458e7f13
Jake Hughey <jakejhughey@gmail.com>
2023-02-03 08:47:37 -0600

    whitespace.

--- install_seeker.sh
+++ install_seeker.sh
@@ -8,4 +8,5 @@ Rscript -e "install.packages('doParallel')"
 Rscript -e "library('seeker')" # force error if install failed
 Rscript -e "library('doParallel')"
 Rscript -e "seeker::installSysDeps('~', '~', '~', '~')"
+
 rm -rf /tmp/downloaded_packages
